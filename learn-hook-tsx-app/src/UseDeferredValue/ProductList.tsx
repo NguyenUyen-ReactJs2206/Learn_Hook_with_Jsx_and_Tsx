44da5dcb632f08587c5c7c1d7c3f696142efaa41
Uyen Nguyen <uyenntt17407@st.uel.edu.vn>
2023-03-21 13:11:15 +0700

    useTransition hook

--- learn-hook-tsx-app/src/UseDeferredValue/ProductList.tsx
+++ learn-hook-tsx-app/src/UseDeferredValue/ProductList.tsx
@@ -1,19 +1,25 @@
-import { useDebugValue, useDeferredValue, useState } from 'react'
+import { useDebugValue, useDeferredValue, useState, useTransition } from 'react'
 import ProductCards from './cpn/ProductCards'
 import ProductInput from './cpn/ProductInput'
 
 export default function ProductList() {
   const [name, setName] = useState<string>('')
+  const [deferredName, setDeferredName] = useState<string>('')
+  const [pending, startTransition] = useTransition()
   const onChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value)
+    startTransition(() => {
+      setDeferredName(event.target.value)
+    })
   }
   //Khi gõ liên tục thì sẽ bị hoãn tý
-  const deferredName = useDeferredValue(name)
+  // const deferredName = useDeferredValue(name)
   return (
     <div>
       <h1> Product List</h1>
       <ProductInput name={name} onChangeName={onChangeName} />
-      <ProductCards name={deferredName} />
+      {pending && <div>Loading....</div>}
+      {!pending && <ProductCards name={deferredName} />}
     </div>
   )
 }
